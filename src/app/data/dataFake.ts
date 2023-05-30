ac44aeec1543e0f9847581cce3b9c60997eb5135
Cleber Farias <jrf.bynho@hotmail.com>
2023-05-30 09:37:08 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "https://www.unimedrio.com.br/wp-content/uploads/2022/04/placeholder-image.jpeg",
+        "title": "saiu novo filme",
+        "description": "ola marvel"
+    },
+    {
+        "id": 2,
+        "photo": "https://www.unimedrio.com.br/wp-content/uploads/2022/04/placeholder-image.jpeg",
+        "title": "saiu novo filme",
+        "description": "ola marvel"
+    }
+]
